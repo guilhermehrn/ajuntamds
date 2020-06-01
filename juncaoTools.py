0e317ba16c6d2f4a8bd97f2c201aee724a3626b7
guibo <guibork@hotmail.com>
2020-06-01 15:15:01 -0300

    correções

--- juncaoTools.py
+++ juncaoTools.py
@@ -7,7 +7,7 @@ from similaridade import Similaridade
 
 class JuncaoTools:
     def __init__(self):
-        self.bd = Dbtool("localhost", "5432", "mds-cad-unico", "postgres", "2631")
+        self.bd = Dbtool("localhost", "5432", "mds_cad_unic", "postgres", "2631")
         self.similar = Similaridade()
 
     def printarLista(self, lista):
@@ -139,64 +139,6 @@ class JuncaoTools:
 
 
 
-        #
-        # self.conjuntoB = self.bd.selecionarTabela([tabelaB], ["*"], '', 0)
-        #
-        # #print("tamanho A:", len(self.conjuntoA))
-        # #print("tamanho B:", len(self.conjuntoB))
-        #
-        #
-        #
-        # self.indexColB = self.bd.retornarColunasIndex(tabelaB)
-        # self.tiposColB = self.bd.retornarColunasTypes(tabelaB)
-        #
-        # resultadosPar = (1, 1, 0.0)
-        #
-        # resultadosFin = []
-        #
-        # #IBGE
-
-        #
-        # #CNEFE
-        #
-        # print ("Fazendo a juncao.")
-        # count = 0
-        # for rowA in self.conjuntoA:
-        #     resultadosPar = (1, 1, 0.0)
-        #     enderecoA = [str(rowA[mEndA[0]]).replace("'",""), str(rowA[mEndA[1]]).replace("'",""), str(rowA[mEndA[2]]).replace("'",""),
-        #                  str(rowA[mEndA[3]]).replace("'",""), str(rowA[mEndA[4]]).replace("'",""), str(rowA[mEndA[5]]).replace("'",""),
-        #                  str(rowA[mEndA[6]]).replace("'","")
-        #                  ]
-        #
-        #     idA = int(rowA[self.indexColA["cod_familiar_fam"]])
-        #     print("dados processados: " + str(count))
-        #     for rowB in self.conjuntoB:
-        #
-        #         enderecoB = [str(rowB[mEndB[0]]).replace("'",""), str(rowB[mEndB[1]]).replace("'",""), str(rowB[mEndB[2]]).replace("'",""),
-        #                      str(rowB[mEndB[3]]).replace("'",""), str(rowB[mEndB[4]]).replace("'",""), str(rowB[mEndB[5]]).replace("'",""),
-        #                      str(rowB[mEndB[6]]).replace("'","")
-        #                      ]
-        #         idb = int(rowB[self.indexColB["cod_unico_endereco"]])
-        #
-        #         #print("END A:", ', '.join(enderecoA))
-        #         #print("END B:", ', '.join(enderecoB))
-        #         diceCoef = self.similar.dice_coefficient2(','.join(enderecoA), ','.join(enderecoB))
-        #
-        #         if diceCoef > resultadosPar[2]:
-        #             resultadosPar = (idA, idb, diceCoef)
-        #     count = count + 1
-        #
-        #     resultadosFin.append(resultadosPar)
-        #
-        # #self.printarLista(resultadosFin)
-        #
-        # arq = open('saida2.csv', 'w')
-        #
-        #
-        # arq.write("cod_familiar_fam; cod_unico_endereco; Dice_coeficiente\n")
-        # for lin in resultadosFin:
-        #     arq.write(str(lin[0]) + ";" + str(lin[1]) + ";" + str(lin[2]) +"\n")
-        # arq.close()
 
 
 j = JuncaoTools()
@@ -208,8 +150,3 @@ fim = time.time()
 
 print("\n=========================================\n")
 print("Tempo: " + str(fim-ini))
-## 1 traz a tabela do CAD do estado MG;
-## 2 tras a tabela agrupada por cidade e quantiade de familhas do CAD
-## for cidade da tabela agrupada traz a tabela da cidade do CNEFE no banco
-## para cada instancia do CAD bucar na tabela do cnefe cidade
-## sempre pegar o maior dice
